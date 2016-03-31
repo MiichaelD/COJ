6cb0fad415c07ac7a9b088775cccacd1693c2a63
Michael Duarte <michael.duarte@hotmail.com>
2016-03-31 15:18:07 -0700

    Added CodingGame_TriForce.cpp
    Creating a triforce triangle

---
+++ CodinGame_TriForce.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ INPUT:
+ N = 1
+ N = 3
+ N = 5
+
+ OUTPUT: 
+.*
+* *
+.    *
+    ***
+   *****
+  *     *
+ ***   ***
+***** *****
+.        *
+        ***
+       *****
+      *******
+     *********
+    *         *
+   ***       ***
+  *****     *****
+ *******   *******
+********* *********
+
+
+
+
+ **/
+int main()
+{
+    int N;
+    cin >> N; cin.ignore();
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    int stars = 1;
+    int smallTriangleBase = (N-1) * 2 + 1;
+    int bigTriangleBase = 2 * smallTriangleBase + 1;
+    int startPos = smallTriangleBase + 1;
+    int accumBreadth = smallTriangleBase;
+    
+    for (int times = 0 ; times < 2 ; ++times){
+        for (int line = 0 ; line < N; ++line){
+            for(int triangles = 0; triangles <= times ; ++triangles){
+                for (int i = 0 ; i < accumBreadth; ++i){// SPACE
+                    cout << (times == 0 && line == 0 && i == 0 ? '.':' ');
+                }    
+                
+                for (int i = 0 ; i < stars; ++i){ // TRIANGLE
+                    cout << '*';
+                }
+                
+                if (times > 0 && triangles == 0){// SPACE - only if we are in the 2nd row and 1st triangle
+                    for (int i = -1 ; i < accumBreadth; ++i){
+                        cout << ' ';
+                    }
+                }
+            }
+            cout << endl;
+            stars +=2;
+            --accumBreadth;
+        }
+        stars = 1;
+    }
+    //
+}
