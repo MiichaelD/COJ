167ca9e8595db94babc1412a1be447997af95350
Michael Duarte <michael.duarte@hotmail.com>
2016-03-31 17:03:49 -0700

    CodingGame_TriForce now support more triangles

--- CodinGame_TriForce.cpp
+++ CodinGame_TriForce.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
  OUTPUT: 
 .*
-* *
+* 
 .    *
     ***
    *****
@@ -33,18 +33,10 @@ using namespace std;
  *******   *******
 ********* *********
 
-
-
-
  **/
-int main()
-{
-    int N;
-    cin >> N; cin.ignore();
 
-    // Write an action using cout. DON'T FORGET THE "<< endl"
-    // To debug: cerr << "Debug messages..." << endl;
 
+void submitedSolution(int N){
     int stars = 1;
     int accumBreadth = (N-1) * 2 + 1;
     
@@ -68,5 +60,45 @@ int main()
             cout << endl;
         }
     }
-    //
+}
+
+
+int main()
+{
+    int N;
+    cin >> N; cin.ignore();
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    int maxTimes = 2; // the challenge just asked for top triangle and 2 below.
+    int stars = 1;
+    int smallestTriangleBase = (N-1) * 2 + 1;
+    int half = N*maxTimes-1;
+//    cout << "Smallest: "<<smallestTriangleBase << ", half: " << half << endl;
+    int accumBreadth = half;
+    
+    for (int times = 0 ; times < maxTimes ; ++times, stars = 1){
+        for (int line = 0 ; line < N; ++line, stars +=2, --accumBreadth){
+            for(int triangles = 0; triangles <= times ; ++triangles){
+                if (triangles  == 0)
+                    for (int i = 0 ; i < accumBreadth; ++i){// SPACE
+                        cout << (times == 0 && line == 0 && i == 0 ? '.':' ');
+                    }    
+                
+                for (int i = 0 ; i < stars; ++i){ // TRIANGLE
+                    cout << '*';
+                }
+                
+                if (triangles < times){// SPACE - only if we are in the 2nd row and 1st triangle
+                    //for (int i = smallestTriangleBase - ((line % N) * 2) ; i > 0 ; --i){ same result as the next line 
+                    for (int i = smallestTriangleBase - stars + 1 ; i > 0 ; --i){
+                        cout << ' ';
+                    }
+                }
+            }
+            cout << endl;
+        }
+    }
+}
 }
